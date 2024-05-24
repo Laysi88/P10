19630146be1ced81332505aba407c07873a2e4d3
Arnaud Lienard <127483177+Laysi88@users.noreply.github.com>
2024-05-24 10:26:59 +0200

    creation pagination

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "CustomUser.CustomUser"
+
+REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination", "PAGE_SIZE": 5}
